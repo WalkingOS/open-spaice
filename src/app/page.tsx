a78450534633a0ce573418574451423d84fe81d7
Osman Minaz <osman.minaz@t-systems.com>
2023-12-07 18:53:33 +0100

    feat: f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,49 @@
 'use client'
 
+import { IFood, IInput, useFetchFood } from '@/lib/hook/food'
+import { useState } from 'react'
+
 export default function Home() {
+  const { loading, food, fetch } = useFetchFood()
+
+  const [input, setInput] = useState<IInput>({
+    country: '',
+    info: '',
+    type: 'breakfast',
+  })
+
+  const handleInputChange = ({ target }) => {
+    setInput((prev) => {
+      return { ...prev, [target.name]: target.value }
+    })
+  }
   return (
     <main className="">
       <h1 className="text-3xl font-semibold mb-6">Get a random recipe!</h1>
-      <form className="bg-gray-200 p-4 rounded-lg shadow-md mb-6 w-full">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          if (input) fetch(input)
+        }}
+        className="bg-gray-200 p-4 rounded-lg shadow-md mb-6 w-full"
+      >
         <label className="block mb-4">
           Origin of the recipe?*
           <input
             required
             type="text"
-            name="recipe"
+            name="country"
+            value={input.country}
             className="block w-full border-gray-300 rounded-md p-2 mt-1"
+            onChange={handleInputChange}
           />
         </label>
         <label className="block mb-4">
           select type*
           <select
+            onChange={handleInputChange}
             name="type"
+            value={input.type}
             className="block w-full border-gray-300 rounded-md p-2 mt-1"
           >
             <option>Breakfast</option>
@@ -25,11 +51,13 @@ export default function Home() {
             <option>Dinner</option>
           </select>
         </label>
-        <label className="block mb-4">
+        <label onChange={handleInputChange} className="block mb-4">
           Additional information
           <input
-            name="text"
+            value={input.info}
+            name="info"
             type="text"
+            onChange={handleInputChange}
             className="block w-full border-gray-300 rounded-md p-2 mt-1"
           />
         </label>
@@ -41,12 +69,40 @@ export default function Home() {
           Go!
         </button>
       </form>
+
+      <section>
+        {loading && <>loading</>}
+        {food && <RecipeCard {...food} />}
+      </section>
     </main>
   )
 }
 
-const RecipeCard = () => {
+const RecipeCard = ({ title, description, receipe, steps }: IFood) => {
   return (
-    <div className="bg-gray-200 p-6 rounded-xl shadow-lg flex flex-col gap-4"></div>
+    <div className="bg-gray-200 p-6 rounded-xl shadow-lg flex flex-col gap-4">
+      <h2 className="font-bold text-xl">{title}</h2>
+      <p className="text-red">{description}</p>
+      <ul className="flex justify-content gap-4 flex-wrap">
+        {receipe.map(({ title, quantity }) => (
+          <li className="bg-white rounded-md" key={title + quantity}>
+            <div className="flex justify-between p-2">
+              <h4>{title}</h4>
+              <p className="ml-4">{quantity}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <ol>
+        {steps.map((step, i) => (
+          <li key={i}>
+            <span>
+              <>{i + 1 + '. ' + step}</>
+            </span>
+          </li>
+        ))}
+      </ol>
+    </div>
   )
 }
---
+++ src/lib/hook/food.ts
@@ -0,0 +1,69 @@
+import OpenAI from "openai";
+import { useState } from "react";
+
+export interface IInput {
+  country: string;
+  type: "breakfast" | "lunch" | "dinner";
+  info: string;
+}
+
+export interface IFood {
+  title: string;
+  description: string;
+  receipe: {
+    title: string;
+    // 200ml, 200g, 2 pieces, etc
+    quantity: string;
+  }[];
+  steps: string[];
+}
+
+export const useFetchFood = () => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const [food, setFood] = useState<IFood | null>(null);
+  const [loading, setLoading] = useState<boolean | null>(null);
+
+  const fetch = async ({ country, type, info }: IInput) => {
+    setLoading(true);
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `You are a helpful cook designed to output JSON. Please give me some nice food receipe
+            from the country ${country},
+            for my ${type},
+            {with this additional information: ${info};
+
+            Please give me a JSON with this SCHEMA:
+              {
+                title: string;
+                description: string;
+                receipe: {
+                  title: string;
+                  // 200ml, 200g, 2 pieces, etc
+                  quantity: string;
+                }[]
+                steps: string[]
+              }
+            `,
+        },
+        { role: "user", content: "Who won the world series in 2020?" },
+      ],
+      model: "gpt-3.5-turbo-1106",
+      response_format: { type: "json_object" },
+      max_tokens: 4096,
+    });
+
+    if (completion.choices[0]?.message.content) {
+      console.log("all message", completion.choices);
+      setFood(JSON.parse(completion.choices[0].message.content));
+      setLoading(false);
+    }
+  };
+
+  return { food, loading, fetch };
+};
